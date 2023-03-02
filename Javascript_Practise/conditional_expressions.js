a029efac14e5f4ce8efacb10aa3d98025187bb90
unstoppablesoni(Deepak Soni) <dsoni2819696@gmail.com>
2023-03-02 13:31:07 +0530

    conditional expression statements in js

---
+++ Javascript_Practise/conditional_expressions.js
@@ -0,0 +1,32 @@
+//if statement
+
+let a = 19;
+if(a>18){
+    console.log("this is a valid age");
+}
+
+//if else statement
+
+let b = 20;
+if(b>18){
+    console.log("this is a valid age")
+}
+else{
+   console.log("not a valid age")
+}
+
+//if else if else statement
+let age = 19;
+
+if(age<18){
+    console.log("not a valid age for driving")
+}
+else if(age>10 && age<15){
+    console.log("you are teen age right now")
+}
+else if(age>18){
+    console.log("you are not a kid, you can drive")
+}
+else{
+    console.log("invalid age")
+}
