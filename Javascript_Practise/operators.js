21a662e22876464557bcb5d9efd07c76b36ad5bc
unstoppablesoni(Deepak Soni) <dsoni2819696@gmail.com>
2023-03-02 13:12:30 +0530

    operators in javascript

---
+++ Javascript_Practise/operators.js
@@ -0,0 +1,17 @@
+//logical operators 
+
+//logical AND &&
+let x = 5;
+let y = 6;
+console.log(x<y && x==5);
+
+//logical OR ||
+let p = 10;
+let q = 5;
+console.log(p<q || q==5);
+
+//logical NOT !
+let a = 5;
+let b = 10;
+console.log(!false);
+console.log(!true);
