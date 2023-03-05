d462a31f1c1d2e17a09660a0fa7620ce6c65c3d3
unstoppablesoni(Deepak Soni) <dsoni2819696@gmail.com>
2023-03-05 18:42:29 +0530

    program of swapping numbers

---
+++ Javascript_Practise/swapping.js
@@ -0,0 +1,9 @@
+//program to swap two numbers without using third variable
+let a = 5;
+let b = 10;
+//output should be a = 10 and b = 5
+a = a+b; //now a will 15
+b = a-b; //now b will 5
+a = a-b; //now a will 10
+console.log(a);
+console.log(b);
