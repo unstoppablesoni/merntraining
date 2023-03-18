3fe1656ab9a1d0765f16a1d4c30d4b185cdf0413
unstoppablesoni(Deepak Soni) <dsoni2819696@gmail.com>
2023-03-18 11:07:15 +0530

    date and time in js

---
+++ Javascript_Practise/date_time.js
@@ -0,0 +1,5 @@
+let abc = new Date();
+console.log(abc);
+
+console.log(new Date().toLocaleString()); //for date and time of your local computer
+console.log(new Date().toString());       // for perfect time of the browser
