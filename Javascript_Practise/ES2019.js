b75e79adc4e827d7e50f4a12b8c98b3534fd2159
unstoppablesoni(Deepak Soni) <dsoni2819696@gmail.com>
2023-03-29 11:58:21 +0530

    ES2019 feature in js

---
+++ Javascript_Practise/ES2019.js
@@ -0,0 +1,10 @@
+//array.flat method to flatten an array.
+
+const arr = [
+    ['zone1','zone2'],
+    ['zone3','zone4'],
+    ['zone5','zone6'],
+    ['zone7',['zone8','zone9']]
+];
+
+console.log(arr.flat(2));
